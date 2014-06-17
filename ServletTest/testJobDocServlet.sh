b58fdc0ab5ac0cbfb6af726db07470699b8e3b14
Pham Tien Thanh <phamtienthanh-luvina@gmail.com>
2014-06-17 15:39:13 +0700

    Update testcase

--- ServletTest/testJobDocServlet.sh
+++ ServletTest/testJobDocServlet.sh
@@ -1,69 +1,77 @@
-if [[ "$#" < 1 ]]; then
+#!/bin/bash
+if [ ! $# -eq 1 ];then
    echo "Incorrect parameter !"
    echo "Usage: ./testJobDocServlet.sh /wiperdog_home_path"
-   exit;
-fi
-wiperdogPath=$1
+   exit
+else
+	wiperdog_home=$1
+	#check if wiperdog directory exists
+	if [ ! -d "$wiperdog_home" ];then
+		echo " >> Wiperdog directory does not exists : $wiperdog_home"
+		exit
+	fi
 
-# Config dbms information
-echo "WRITING DBMS INFORMATION INTO: " $wiperdogPath/etc/dbms_info.cfg
-cat > $wiperdogPath/etc/dbms_info.cfg <<eof
-	[
-		"DbType": [
-			"MySQL": "@MYSQL",
-			"SQL_Server": "@MSSQL",
-			"Postgres": "@PGSQL",
-			"MongoDB": "@MONGO",
-			"MariaDB": "@MARIA"
-		],
-		"TreeMenuInfo": [
-			"MySQL": [
-				"Database_Area":[],
-				"Database_Statistic":[],
-				"Database_Structure":[],
-				"FaultManagement":[],
-				"Performance":[],
-				"Proactive_Check":[],
-				"Others":[]
+	# Config dbms information
+	echo "WRITING DBMS INFORMATION INTO: " $wiperdog_home/etc/dbms_info.cfg
+	cat > $wiperdog_home/etc/dbms_info.cfg <<eof
+		[
+			"DbType": [
+				"MySQL": "@MYSQL",
+				"SQL_Server": "@MSSQL",
+				"Postgres": "@PGSQL",
+				"MongoDB": "@MONGO",
+				"MariaDB": "@MARIA"
 			],
-			"SQL_Server":[
-				"Database_Area":[],
-				"Database_Statistic":[],
-				"Database_Structure":[],
-				"FaultManagement":[],
-				"Performance":[],
-				"Proactive_Check":[],
+			"TreeMenuInfo": [
+				"MySQL": [
+					"Database_Area":[],
+					"Database_Statistic":[],
+					"Database_Structure":[],
+					"FaultManagement":[],
+					"Performance":[],
+					"Proactive_Check":[],
+					"Others":[]
+				],
+				"SQL_Server":[
+					"Database_Area":[],
+					"Database_Statistic":[],
+					"Database_Structure":[],
+					"FaultManagement":[],
+					"Performance":[],
+					"Proactive_Check":[],
+					"Others":[]
+				],
+				"Postgres":[
+					"Database_Area":[],
+					"Database_Statistic":[],
+					"Database_Structure":[],
+					"FaultManagement":[],
+					"Performance":[],
+					"Proactive_Check":[],
+					"Others":[]
+				],
+				"MongoDB":[
+					"Database_Area":[],
+					"Database_Statistic":[],
+					"Database_Structure":[],
+					"FaultManagement":[],
+					"Performance":[],
+					"Proactive_Check":[],
+					"Others":[]
+				],
+				"OS":[],
 				"Others":[]
-			],
-			"Postgres":[
-				"Database_Area":[],
-				"Database_Statistic":[],
-				"Database_Structure":[],
-				"FaultManagement":[],
-				"Performance":[],
-				"Proactive_Check":[],
-				"Others":[]
-			],
-			"MongoDB":[
-				"Database_Area":[],
-				"Database_Statistic":[],
-				"Database_Structure":[],
-				"FaultManagement":[],
-				"Performance":[],
-				"Proactive_Check":[],
-				"Others":[]
-			],
-			"OS":[],
-			"Others":[]
+			]
 		]
-	]
 eof
 
-echo "WRITE OK, RESTARTING WIPERDOG !!!"
-fuser -k 13111/tcp
-/bin/sh $wiperdogPath/bin/startWiperdog.sh > /dev/null 2>&1 &
-sleep 30
-echo "WIPERDOG WAS STARTED !!!"
+	# Restart wiperdog to apply new configuration of dbms
+	echo "** STARTING WIPERDOG ..."
+	fuser -k 13111/tcp
+	/bin/sh $wiperdog_home/bin/startWiperdog.sh > /dev/null 2>&1 &
+	sleep 30
+	echo "** WIPERDOG WAS RUNNING ..."
+fi
 
 echo ">>>>> TEST GET METHOD OF JobDocServlet <<<<<"
 content=$(curl -i -H "Accept: application/json" -H "Content-Type: application/json" 'http://localhost:13111/JobDocInfoServlet')
